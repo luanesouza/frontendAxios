2cb8af4042a8343a550bd1d6dbb9c10dcd9a9a06
luanesouza <luanesouza411@gmail.com>
2020-05-21 11:40:48 -0400

    removes extra code

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,18 @@
-const axios = require('axios');
+// const axios = require('axios');
+import axios from 'axios';
 const url = 'http://localhost:3000/people'
 
 
 
-
 const getPeopleFetch = () => {
+  return fetch(url)
+  .then( response => response.json())
+  .then( data => {return data})
 }
 
-const getPeopleAxios = () => {
+const getPeopleAxios = async () => {
+  let response = await axios(url);
+  return response.data;
 }
-
-
+getPeopleFetch()
 export { getPeopleFetch, getPeopleAxios }
